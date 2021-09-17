3e3d09e3ab05e6212fa95a1bbd2caeede8ca62ae
anson10 <sansonmsa@gmail.com>
2021-09-17 23:30:45 +0530

    New files added

--- script.py
+++ script.py
@@ -1,2 +1,10 @@
-my_file = open('test.txt')
-print(my_file.read())
+from translate import Translator
+translate = Translator(to_lang="ja")
+try:
+    with open('test.txt', mode='r') as my_file:
+        text = my_file.read()
+        translation = translate.translate(text)
+        with open('test-ja.txt', mode='w') as my_file2:
+            my_file2.write(translation)
+except FileNotFoundError as e:
+    print("Check your path!")
